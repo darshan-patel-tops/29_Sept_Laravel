003ced43f77adaf66822e092dab27cceed08e553
darshan <darshan.patel.tops@gmail.com>
2023-08-09 09:43:03 +0530

    update

---
+++ JavaScript/variable_basic.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        // alert("login success")
+        // confirm("are you sure??")
+        // prompt("Enter addition of 2+2 =")
+        // let res = prompt("Enter addition of 2+2 =")
+        // let res = prompt("Enter addition of 2+2 =") // let dnt allowed redeclaration allows re-assign preferable by memery allocation 
+        // var res = prompt("Enter addition of 2+2 =") // let allows redeclaration allows re-assign
+        console.log(res);
+        var res = prompt("Enter addition of 2+2 =") 
+        // let res = prompt("Enter addition of 2+2 =") 
+        // const res = 3.14 //pi
+    </script>
+</body>
+</html>
