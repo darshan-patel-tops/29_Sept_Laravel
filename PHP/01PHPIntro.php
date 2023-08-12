fcab61411978ca1f3fb465a63fab84499aa658a7
darshan <darshan.patel.tops@gmail.com>
2023-08-12 16:14:13 +0530

    Update

---
+++ PHP/01PHPIntro.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h2>PHP</h2>
+
+<ul>
+    <li>PHP is a server side scripting lang</li>
+    <li>XAMPP => Pacakege (X=>Cross Plateform, A => Apache, M => MariaDB, P=>PHP, P=> pearl)</li>
+    <li>will access by localhost  => Store data inside htdocs</li>
+    <li>Invented in 1994 rusmus ledorf</li>
+    <li>Published in 1995</li>
+    <li>PHP => Hypertext PreProcessor earlier it's called as Personal Home Page because that time it was useed for uploading resume online</li>
+    <li>PHP => 2004 ZEND engine zesuraski and andigotmus</li>
+    <li>PHP => 2005 OOPS</li>
+</ul>
+
+<p>PHP supports HTML but HTML doesnt supports the PHP thats why we aleays create .php extension pages</p>
+
+</body>
+</html>
