0381ad29c1be96bf64b91b70ebd82f9eeee6453e
darshan <darshan.patel.tops@gmail.com>
2023-08-14 08:41:29 +0530

    Update

---
+++ PHP/02variable.php
@@ -0,0 +1,17 @@
+<?php
+
+// error_reporting(0);
+// vari+able
+$a = 50;
+// echo $a+"150";
+// echo $a+"150aaa";
+echo $a+80;
+$username="TOPS";
+echo $username; 
+echo $username1213; 
+echo $user_name1213; 
+// echo $132a; //wrong
+// echo $user name; //wrong
+// echo $user$%^&^%$#name; //wrong
+
+?>
