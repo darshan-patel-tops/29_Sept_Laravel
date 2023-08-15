8eefe84de1e4fc41e04f98f2460e19bdd330ae0d
darshan <darshan.patel.tops@gmail.com>
2023-08-15 13:07:26 +0530

    Update

---
+++ PHP/04Operators.php
@@ -0,0 +1,18 @@
+<?php
+
+// arithmatic op + - */
+// Ternary ? : (alternatrive of if else)
+// compar 
+$marks = 50;
+//++ --
+$marks = 50;
+if ($marks > 50) {
+    echo "called if";
+} else {
+    echo "called else";
+}
+echo ($marks > 50)? "called if":"called else";
+
+echo  55<=>60; 
+
+?>
